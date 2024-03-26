1ba70035c76bf4013713e404c774b3e24bc7c574
SteveJobs-2 <88567996+SteveJobs-2@users.noreply.github.com>
2024-03-26 18:46:55 +0800

    Modify template.css file for more optimal navbar

--- src/pages/template.js
+++ src/pages/template.js
@@ -12,29 +12,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Template(){
     return(
         <Navbar expand="lg" className="bg-body-tertiary">
-              <Container>
-                <Navbar.Brand href="#home">Lost & Found</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/lostItemNotice">Lost Item Notice</Nav.Link>
-                    <Nav.Link href="/lostItemCatalog">Lost Item Catalog</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.2">
-                        Another action
-                      </NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
+            <Container>
+            <Navbar.Brand style = {{position:"relative", right:180}} href="/home">Lost & Found</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" style = {{position:"relative", left:880}}>
+                <Nav className="me-auto">
+                <Nav.Link className ="headerLink" href="/home">Home</Nav.Link>
+                <Nav.Link className ="headerLink" href="/lostItemNotice">Lost Item Notice</Nav.Link>
+                <Nav.Link className ="headerLink" href="/lostItemCatalog">Lost Item Catalog</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Home</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Lost Item Notice</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Lost Item Catalog</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
