417c52b8438d16a6dbf4f6b501d746e79a6069b2
Mao <luomaoyuan2001@gmail.com>
2024-03-28 18:04:12 +0800

    Modify template.js & template.css

--- src/pages/template.js
+++ src/pages/template.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
+import {Container, Card} from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -11,20 +11,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Template(){
     return(
-        <Navbar expand="lg" className="justify-content-center navbar-dark navbar-custom">
+        // Navigation Bar
+        <>
+            <Navbar expand="lg" className="justify-content-center navbar-dark navbar-custom">
+                <Container fluid>
+                <Navbar.Brand className="navbar-custom" href="/home">Lost & Found</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+                <Navbar.Collapse id="basic-navbar-nav">
+                <NavDropdown title="Dropdown" variant="success" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/home">Home</NavDropdown.Item>
+                    <NavDropdown.Item href="/lostItemNotice">Lost Item Notice</NavDropdown.Item>
+                    <NavDropdown.Item href="/lostItemCatalog">Lost Item Catalog</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                </NavDropdown>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/* Footer */}
             <Container fluid>
-            <Navbar.Brand className="navbar-custom" href="/home">Lost & Found</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />  
-            <Navbar.Collapse id="basic-navbar-nav">
-            <NavDropdown title="Dropdown" variant="success" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/home">Home</NavDropdown.Item>
-                <NavDropdown.Item href="/lostItemNotice">Lost Item Notice</NavDropdown.Item>
-                <NavDropdown.Item href="/lostItemCatalog">Lost Item Catalog</NavDropdown.Item>
-                <NavDropdown.Divider />
-            </NavDropdown>
-            </Navbar.Collapse>
+                <Card body className="justify-content-center navbar-custom bottom">Hi</Card>
             </Container>
-        </Navbar>
+        </>
     );
 }
 
