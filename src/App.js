78cd83b2a5d8afb3fe829e7aba6d208113a48897
SteveJobs-2 <88567996+SteveJobs-2@users.noreply.github.com>
2024-03-28 23:20:13 +0800

    Modify settings.js and settings.css files

--- src/App.js
+++ src/App.js
@@ -9,56 +9,6 @@ import Template from './pages/template';
 import ContactFinder from './pages/contactFinder';  
 import Settings from './pages/settings';
 
-// class App extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//           users: []
-//         }
-//     }
-
-//     componentDidMount() {
-
-//       fetch('http://localhost:5000/api', {
-//         method: "GET",
-//         mode: "cors",
-//         headers: {
-//           "Content-Type":"application/json",
-//         }
-//       })
-//       .then(response => response.json())
-//       .then(res => {
-//         //check if response has data
-//           if (res.data) {
-//             this.setState({ users: [...this.state.users, ...res.data] })
-//           }
-//       });
-
-//     }
-
-//     renderUsers() {
-
-//       if (this.state.users.length <= 0) {
-//         return <div>loading...</div>
-//       }
-//       else {
-//         return this.state.users.map((val, key) => {
-//           return <div key={key}>{val.name} | {val.age}</div>
-//         })
-//       }
-
-//     }
-
-//     render() {
-//       return (
-//         <div className="App">
-//           { this.renderUsers() }
-//         </div>
-//       );
-//     }
-
-// }
 
 function Apps(){
   return (
@@ -79,56 +29,5 @@ function Apps(){
     </div>
   );
 }
-// function App() {
-
-//   //declare backendData as a state variable
-//   const [backendData, setBackendData] = useState([{}]);
-
-//   //inside the useEffect hook, we set the backendData state variable after fetching the data
-//   //from the backend via api call
-//   useEffect(() => {
-//     fetch('/api').then(
-//       //response will be the res.json from the api call
-//       response => response.json()
-//     ).then(
-//       data => {
-//         setBackendData(data);
-//       }
-//     );
-//     //[] tells useEffect block to run only after the first render
-//   }, []);
-
-//   getData();
-
-//   return (
-
-//     //if backendData is undefined(api fetch not working), render a loading on the screen
-//     //else if api fetch has worked, map the users to a <p> and display them
-//     <div>
-      
-//       {(typeof backendData.users === 'undefined') ? (
-//         <p>Loading...</p>
-//       ) : (
-//         backendData.users.map((user, i) => (
-//           <p key={i}>{user}</p>
-//         ))
-//       )}
-
-//     </div>
-//   );
-// }
-
-// async function getData() {
-
-//   const request = new Request('/api', {
-//     method: 'GET'
-//   })
-
-//   const res = await fetch(request);
-//   const data = await res.json()
-//   console.log(res)
-//   console.log(data)
-
-// }
 
 export default Apps;
--- src/pages/settings.js
+++ src/pages/settings.js
@@ -3,11 +3,49 @@ import axios from 'axios';
 import {useEffect, useState} from 'react'; // Used for api calls to server
 import {useNavigate} from 'react-router-dom';
 import {Formik, Form, Field, ErrorMessage} from 'formik';
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './settings.css';
 
 function Settings(){
-    return;
+    return(
+        <>
+            <div className="text-settings"><h1>Settings</h1></div>
+            <Accordion className="accordion-settings col-sm-4 mx-auto">
+                <Accordion.Item eventKey="0" className="accordion-item-settings">
+                    <Accordion.Header>What do i do if i lost an item?</Accordion.Header>
+                    <Accordion.Body>
+                    Click lost an item. You can access the map to see if your item has been found or submit a lost item notice
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1" className="accordion-item-settings">
+                    <Accordion.Header>What do i do if i found an item?</Accordion.Header>
+                    <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2" className="accordion-item-settings">
+                    <Accordion.Header>How do i contact the person who found my item?</Accordion.Header>
+                    <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </>
+    )
 }
 
 export default Settings;
