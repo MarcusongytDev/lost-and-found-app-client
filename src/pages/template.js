d03d9324b221e421314612213f198a39e77d7c52
Mao <luomaoyuan2001@gmail.com>
2024-03-28 17:33:46 +0800

    Modify template.js file

--- src/pages/template.js
+++ src/pages/template.js
@@ -11,17 +11,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Template(){
     return(
-        <Navbar expand="lg" className="bg-body-tertiary justify-content-center">
-            <Container>
-            <Navbar.Brand  href="/home">Lost & Found</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" style = {{position:"relative", left:880}}>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/home">Home</NavDropdown.Item>
-                    <NavDropdown.Item href="/lostItemNotice">Lost Item Notice</NavDropdown.Item>
-                    <NavDropdown.Item href="/lostItemCatalog">Lost Item Catalog</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                </NavDropdown>
+        <Navbar expand="lg" className="justify-content-center navbar-dark navbar-custom">
+            <Container fluid>
+            <Navbar.Brand className="navbar-custom" href="/home">Lost & Found</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />  
+            <Navbar.Collapse id="basic-navbar-nav">
+            <NavDropdown title="Dropdown" variant="success" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/home">Home</NavDropdown.Item>
+                <NavDropdown.Item href="/lostItemNotice">Lost Item Notice</NavDropdown.Item>
+                <NavDropdown.Item href="/lostItemCatalog">Lost Item Catalog</NavDropdown.Item>
+                <NavDropdown.Divider />
+            </NavDropdown>
             </Navbar.Collapse>
             </Container>
         </Navbar>
