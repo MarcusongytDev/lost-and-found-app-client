fc0a01689a22bf01bebcff3327c1674b24acd346
Xatan01 <149221783+Xatan01@users.noreply.github.com>
2024-03-28 16:32:53 +0800

    Added NewHomePage
    Need to change the formatting a bit

--- src/App.js
+++ src/App.js
@@ -1,128 +1,52 @@
-import React, { Component, useEffect, useState } from 'react';
-import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; // Install components, router, route
-import Home from "./pages/home";
-import lostItemCatalog from "./pages/lostItemCatalog";
-import LostItemNotice from './pages/lostItemNotice';
-import Template from './pages/template';
+import React, { Component } from 'react';
+import './App.css'; // Import the CSS file for styling
+// Import the BasicExample component
+import NewHomePage from './pages/NewHomePage';
 
-// class App extends Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//           users: []
-//         }
-//     }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
 
-//     componentDidMount() {
+  componentDidMount() {
+    fetch('http://localhost:5000/api', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(res => {
+        if (res.data) {
+          this.setState({ users: [...this.state.users, ...res.data] });
+        }
+      });
+  }
 
-//       fetch('http://localhost:5000/api', {
-//         method: "GET",
-//         mode: "cors",
-//         headers: {
-//           "Content-Type":"application/json",
-//         }
-//       })
-//       .then(response => response.json())
-//       .then(res => {
-//         //check if response has data
-//           if (res.data) {
-//             this.setState({ users: [...this.state.users, ...res.data] })
-//           }
-//       });
+  renderUsers() {
+    if (this.state.users.length <= 0) {
+      return <div>loading...</div>;
+    } else {
+      return this.state.users.map((val, key) => {
+        return <div key={key}>{val.name} | {val.age}</div>;
+      });
+    }
+  }
 
-//     }
-
-//     renderUsers() {
-
-//       if (this.state.users.length <= 0) {
-//         return <div>loading...</div>
-//       }
-//       else {
-//         return this.state.users.map((val, key) => {
-//           return <div key={key}>{val.name} | {val.age}</div>
-//         })
-//       }
-
-//     }
-
-//     render() {
-//       return (
-//         <div className="App">
-//           { this.renderUsers() }
-//         </div>
-//       );
-//     }
-
-// }
-
-function Apps(){
-  return (
-    <div className="App">
-      <Router>
-        <Template/>
-        <Routes>
-          <Route path="/">
-            <Route path = "home" exact element={<Home/>} />
-            <Route path = "lostItemNotice" exact element={<LostItemNotice/>} />
-            <Route path = "lostItemCatalog" exact element={<lostItemCatalog/>} /> {/* id entered in path inputed into Post file by useParams() */}
-          </Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {this.renderUsers()} 
+        {/* Render the BasicExample component here */}
+        <NewHomePage />
+      </div>
+    );
+  }
 }
-// function App() {
 
-//   //declare backendData as a state variable
-//   const [backendData, setBackendData] = useState([{}]);
-
-//   //inside the useEffect hook, we set the backendData state variable after fetching the data
-//   //from the backend via api call
-//   useEffect(() => {
-//     fetch('/api').then(
-//       //response will be the res.json from the api call
-//       response => response.json()
-//     ).then(
-//       data => {
-//         setBackendData(data);
-//       }
-//     );
-//     //[] tells useEffect block to run only after the first render
-//   }, []);
-
-//   getData();
-
-//   return (
-
-//     //if backendData is undefined(api fetch not working), render a loading on the screen
-//     //else if api fetch has worked, map the users to a <p> and display them
-//     <div>
-      
-//       {(typeof backendData.users === 'undefined') ? (
-//         <p>Loading...</p>
-//       ) : (
-//         backendData.users.map((user, i) => (
-//           <p key={i}>{user}</p>
-//         ))
-//       )}
-
-//     </div>
-//   );
-// }
-
-// async function getData() {
-
-//   const request = new Request('/api', {
-//     method: 'GET'
-//   })
-
-//   const res = await fetch(request);
-//   const data = await res.json()
-//   console.log(res)
-//   console.log(data)
-
-// }
-
-export default Apps;
+export default App;
---
+++ src/pages/NewHomePage.js
@@ -0,0 +1,65 @@
+import "./NewHomePage.css";
+
+const NewHomePage = () => {
+  return (
+    <div className="new-home-page">
+      <img className="new-home-page-child" alt="" src="/line-39.svg" />
+      <img className="new-home-page-item" alt="" src="/line-39.svg" />
+      <img
+        className="emile-seguin-r9ouekotggu-unspl-icon"
+        alt=""
+        src="/emileseguinr9ouekotgguunsplash-1@2x.png"
+      />
+      <div className="frame">
+        <div className="content">
+          <b className="never-lose-again">Never Lose Again With Us</b>
+          <div className="we-are-committed">
+            We are committed to building a lost free Singapore society
+          </div>
+          <div className="primary-button">
+            <b className="looking-for-an">Looking For An Item</b>
+          </div>
+          <div className="primary-button1">
+            <b className="found-an-item">Found An Item</b>
+          </div>
+        </div>
+      </div>
+      <img className="frame-icon" alt="" src="/frame.svg" />
+      <div className="frame1">
+        <div className="version-10-group-container">
+          <p className="version-10">Version 1.0 Group Varghes</p>
+        </div>
+      </div>
+      <img className="new-home-page-inner" alt="" src="/line-39.svg" />
+      <img className="line-icon" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child1" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child" alt="" src="/line-39.svg" />
+      <div className="rectangle-div" />
+      <img className="new-home-page-child3" alt="" src="/line-39.svg" />
+      <div className="rectangle-div" />
+      <div className="rectangle-div" />
+      <img className="new-home-page-item" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child3" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child" alt="" src="/line-39.svg" />
+      <img className="new-home-page-item" alt="" src="/line-39.svg" />
+      <img className="frame-icon" alt="" src="/frame.svg" />
+      <div className="frame1">
+        <div className="version-10-group-container">
+          <p className="version-10">Version 1.0 Group Varghes</p>
+        </div>
+      </div>
+      <img className="new-home-page-inner" alt="" src="/line-39.svg" />
+      <img className="line-icon" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child1" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child" alt="" src="/line-39.svg" />
+      <div className="rectangle-div" />
+      <img className="new-home-page-child3" alt="" src="/line-39.svg" />
+      <div className="rectangle-div" />
+      <div className="rectangle-div" />
+      <img className="new-home-page-item" alt="" src="/line-39.svg" />
+      <img className="new-home-page-child3" alt="" src="/line-39.svg" />
+    </div>
+  );
+};
+
+export default NewHomePage;
