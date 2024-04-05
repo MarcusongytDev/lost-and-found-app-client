267f6e53db38a82b2099aafea2d0603002b2c8f7
SteveJobs-2 <88567996+SteveJobs-2@users.noreply.github.com>
2024-04-05 16:32:07 +0800

    Update google maps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; // Install components, router, route
+import {BrowserRouter as Router, Routes, Route, Link, Navigate} from 'react-router-dom'; // Install components, router, route
 import NewHomePage from "./pages/NewHomePage";
 import LostItemCatalog from "./pages/lostItemCatalog";
 import LostItemNotice from './pages/lostItemNotice';
@@ -14,11 +14,13 @@ import LostItemMap from './pages/lostItemMap';
 
 
 function Apps(){
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/">
+            <Route path = "" exact element={<Navigate to="home" />} />
             <Route path = "home" exact element={<><Template/><NewHomePage/></>} />
             <Route path = "lostItemNotice" exact element={<><Template/><LostItemNotice/></>} />
             <Route path = "lostItemCatalog" exact element={<><Template/><LostItemCatalog/></>} />
--- src/components/API/GoogleMaps.js
+++ src/components/API/GoogleMaps.js
@@ -8,13 +8,13 @@ import {
 } from "@vis.gl/react-google-maps";
 
 export default function GoogleMaps() {
-  const position = { lat: 103.8198, lng: 1.3521 };
+  const position = { lat: 1.349, lng: 103.739 };
   const [open, setOpen] = useState(false);
 
   return (
     <APIProvider apiKey="AIzaSyB5yzIMiOUagFda-20MnNBruQAGgdsVPfc">
       <div style={{ height: "100vh", width: "100%" }}>
-        <Map zoom={9} center={position} mapId={"95ff34d67269854f"}>
+        <Map zoom={14} center={position} mapId={"95ff34d67269854f"}>
           <AdvancedMarker position={position} onClick={() => setOpen(true)}>
             <Pin
               background={"grey"}
@@ -25,7 +25,7 @@ export default function GoogleMaps() {
 
           {open && (
             <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-              <p>I'm in Hamburg</p>
+              <p>Test</p>
             </InfoWindow>
           )}
         </Map>
