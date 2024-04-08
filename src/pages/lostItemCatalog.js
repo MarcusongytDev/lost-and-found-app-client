31943407ad4e0b4f296222be925307bbda1ec63e
rheasgeorge <131263849+rheasgeorge@users.noreply.github.com>
2024-04-08 17:16:26 +0800

    modify Lost Item Catalog Page

--- src/pages/lostItemCatalog.js
+++ src/pages/lostItemCatalog.js
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './lostItemCatalog.css';
 import LIC_Card from "../components/UI/LIC_Card";
 import tempImage from "../assets/questionmark-icon.png";
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -30,28 +31,48 @@ function LostItemCatalog() {
       navigate(path);
     }
 
-    return (
-        <>
-          {/* Header */}
-          <Container fluid style={{textAlign: "center", paddingTop: "10px"}}>
-            <h1>Lost Items Reported</h1>
-          </Container>
-          {/* Item Catalog */}
-          <Container fluid className="LIC-Catalog" style={{paddingTop: "20px"}}>
-            <Row>
-              <Col xs={4} className="LIC-Flexbox">
-                <LIC_Card/>
-              </Col>
-              <Col xs={4} className="LIC-Flexbox">
-                <LIC_Card/>
-              </Col>
-              <Col xs={4} className="LIC-Flexbox">
-                <LIC_Card/>
-              </Col>
-            </Row>
-          </Container>
-        </>
-      );
+return (
+  <>
+    {/* Header with Filter Bar */}
+    <Container fluid style={{textAlign: "center", paddingTop: "10px"}}>
+      <Row>
+        <Col md={8}>
+          <h1>Lost Items Reported</h1>
+        </Col>
+        <Col md={4} className="d-flex align-items-center justify-content-end">
+          <Form.Control type="text" placeholder="Keyword Filter" className="me-2" />
+        </Col>
+      </Row>
+    </Container>
+
+    {/* Item Catalog */}
+    <Container fluid className="LIC-Catalog" style={{paddingTop: "20px"}}>
+      <Row>
+        <Col xs={4} className="LIC-Flexbox">
+          <LIC_Card/>
+        </Col>
+        <Col xs={4} className="LIC-Flexbox">
+          <LIC_Card/>
+        </Col>
+        <Col xs={4} className="LIC-Flexbox">
+          <LIC_Card/>
+        </Col>
+      </Row>
+    </Container>
+
+    {/* Buttons */}
+    <Container fluid style={{textAlign: "center", margin: "20px 0"}}>
+      <Row>
+        <Col>
+          <Button variant="primary" href="/lostItemMap" onClick={() => routeChange('/map-view')}>Return to Map View</Button>
+        </Col>
+        <Col>
+          <Button variant="success" href="/lostItemNotice" onClick={() => routeChange('/post-notice')}>Can't Find Item, Post a Notice Now!</Button>
+        </Col>
+      </Row>
+    </Container>
+  </>
+);
 }
 
 export default LostItemCatalog;
