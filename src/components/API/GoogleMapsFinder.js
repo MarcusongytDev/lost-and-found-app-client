49181cefe0a60eb008f41ab1dd26955ba60dc34a
Xatan01 <149221783+Xatan01@users.noreply.github.com>
2024-04-08 20:38:34 +0800

    Changed FoundItemNotice and GoogleMaps API call for it

---
+++ src/components/API/GoogleMapsFinder.js
@@ -0,0 +1,113 @@
+import { useEffect, useState} from "react";
+import {useLoadScript} from "@react-google-maps/api";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+import usePlacesAutoComplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import './GoogleMapsFinder.css'
+
+export default function GoogleMaps() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyB5yzIMiOUagFda-20MnNBruQAGgdsVPfc",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <GoogleMap />;
+}
+
+const SINGAPORE_BOUNDS = {
+  north: 1.21013,
+  south: 103.60567,
+  west: 1.47156,
+  east: 104.04364
+};
+
+function GoogleMap() {
+  const [open, setOpen] = useState(false);
+  const position = { lat: 1.349, lng: 103.739 }
+  // Get places library in google maps api
+
+  // Selected allows us to pass a location and render it as a market on the map
+  const [selected, setSelected] = useState({ lat: 1.349, lng: 103.739 });
+
+  return (
+    <APIProvider>
+      <PlacesAutocomplete className="Gmaps-Searchbar" setSelected = {setSelected}/>
+      <Map defaultZoom={15} defaultCenter={selected} mapId={"95ff34d67269854f"} className="map-container">
+        <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+          <Pin
+            background={"grey"}
+            borderColor={"green"}
+            glyphColor={"purple"}
+          />
+        </AdvancedMarker>
+
+        {open && (
+          <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+            <p>Test</p>
+          </InfoWindow>
+        )}
+        {selected && (
+          <>
+            <AdvancedMarker position={selected}>
+              <Pin />
+            </AdvancedMarker>
+            <InfoWindow disableAutoPan={false} position={selected} ><p className="disable-opacity">test</p></InfoWindow>
+          </>
+        )
+        }
+      </Map>
+    </APIProvider>
+  );
+}
+
+const PlacesAutocomplete = ({setSelected}) =>{
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: {status, data},
+    clearSuggestions,
+  } = usePlacesAutoComplete();
+
+  const handleSelect = async(address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({address});
+    const {lat, lng} = await getLatLng(results[0]);
+    setSelected({lat, lng});
+  }
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)} 
+        disabled={!ready} 
+        className="combobox-input-finder" 
+        placeholder="Search For Address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" && data.map(({place_id, description}) => <ComboboxOption key={place_id} value={description}/>)}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  )
+}
--- src/pages/foundItemNotice.js
+++ src/pages/foundItemNotice.js
@@ -1,114 +1,175 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { WithContext as ReactTags } from 'react-tag-input';
 import * as Yup from 'yup';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './foundItemNotice.css';
+import GoogleMaps from "../components/API/GoogleMapsFinder.js"; // Import the GoogleMaps component
 
 function FoundItemNotice() {
-  const navigate = useNavigate();
-  const [dateFound, setDateFound] = useState('');
-  const [timeFound, setTimeFound] = useState('');
+    const [dateFound, setDateFound] = useState('');
+    const [timeFound, setTimeFound] = useState('');
+    const [selectedLocation, setSelectedLocation] = useState(null); // State to store the selected location
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState(''); // State for description field
+    const [email, setEmail] = useState(''); // State for email field
+    const [phone, setPhone] = useState(''); // State for phone number field
+    const [tags, setTags] = useState([]); // Initialize tags state for itemFilter tags
 
-  const initialValues = {
-    category: '',
-    color: '',
-    photo: null // New field for photo upload
-  };
+    const initialValues = {
+        name: '',
+        datefound: '',
+        timefound: '',
+        description: '',
+        email: '',
+        phone: '',
+        photo: null,
+        itemFilter: [] // Initialize itemFilter array for tags
+    };
 
-  const validationSchema = Yup.object({
-    category: Yup.string().required('Required'),
-    color: Yup.string().required('Required'),
-    photo: Yup.mixed().required('Photo of item is required') // Validation for photo upload
-  });
+    const validationSchema = Yup.object({
+        name: Yup.string().required('Name is required'),
+        datefound: Yup.date().required('Date Found is required'),
+        timefound: Yup.string().required('Time Found is required'),
+        description: Yup.string().required('Description is required'),
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        phone: Yup.string().matches(/^[0-9]+$/, 'Must be a valid phone number').required('Phone number is required'),
+        photo: Yup.mixed().required('Photo of item is required')
+    });
 
-  const onSubmit = async (values) => {
-    const datetimeFound = `${dateFound} ${timeFound}`;
-    const formData = new FormData();
-    formData.append('category', values.category);
-    formData.append('color', values.color);
-    formData.append('datetimefound', datetimeFound);
-    formData.append('photo', values.photo); // Append photo to form data
-    console.log(values);
-    try {
-      await axios.post('http://localhost:5000/post-lost-item', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
+    const onSubmit = async (values) => {
+        // Submit form data
+        try {
+            // Update the selected location directly on form submission
+            setSelectedLocation({ latitude: 123, longitude: 456 }); // Replace with the actual selected location
+            // Assuming you have a function to get the selected location from the map component
+            await axios.post('http://localhost:5000/founditems', {
+                ...values,
+                location: { latitude: 123, longitude: 456 }, // Add selected location to form data
+                name: name // Add name to form data
+            });
+        } catch (error) {
+            console.error('There was an error submitting the form:', error);
         }
-      });
-      navigate('/home');
-    } catch (error) {
-      console.error('There was an error submitting the form:', error);
-    }
-  };
+    };
 
-  return (
-    <div className='backgroundsettings'>
-      <div className="LIN-container my-5">
-        <h2 className="LIN-heading text-center mb-4">Details of Found Item</h2>
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-          {({ values, setFieldValue }) => ( // Pass setFieldValue to handle file changes
-            <Form>
-              <div className="form-group">
-                <label htmlFor="category">Item Category</label>
-                <Field id="category" name="category" type="text" className="form-control LIN-formControl" />
-                <ErrorMessage name="category" component="div" className="LIN-errorMessage" />
-              </div>
+    const handleDelete = (i) => {
+        setTags(tags.filter((tag, index) => index !== i));
+    };
 
-              <div className="form-group">
-                <label htmlFor="color">Color</label>
-                <Field id="color" name="color" type="text" className="form-control LIN-formControl" />
-                <ErrorMessage name="color" component="div" className="LIN-errorMessage" />
-              </div>
+    const handleAddition = (tag) => {
+        setTags([...tags, tag]);
+    };
 
-              <div className="form-group">
-                <label htmlFor="datefound">Date Found</label>
-                <input
-                  id="datefound"
-                  name="datefound"
-                  type="date"
-                  value={dateFound}
-                  onChange={(e) => setDateFound(e.target.value)}
-                  className="form-control LIN-formControl"
-                />
-                <ErrorMessage name="datefound" component="div" className="LIN-errorMessage" />
-              </div>
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = [...tags];
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
+        setTags(newTags);
+    };
 
-              <div className="form-group">
-                <label htmlFor="timefound">Time Found</label>
-                <input
-                  id="timefound"
-                  name="timefound"
-                  type="time"
-                  value={timeFound}
-                  onChange={(e) => setTimeFound(e.target.value)}
-                  className="form-control LIN-formControl"
-                />
-                <ErrorMessage name="timefound" component="div" className="LIN-errorMessage" />
-              </div>
+    return (
+        <div className='backgroundsettings'>
+            <div className="LIN-container my-5">
+                <h2 className="LIN-heading text-center mb-4">Details of Found Item</h2>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                    {({ values, setFieldValue }) => (
+                        <Form>
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <Field id="name" name="name" type="text" className="form-control LIN-formControl" />
+                                <ErrorMessage name="name" component="div" className="LIN-errorMessage" />
+                            </div>
+                            <GoogleMaps selectedLocation={selectedLocation} /> {/* Render the GoogleMaps component */}
 
-              <div className="form-group">
-                <label htmlFor="photo">Photo of Item</label>
-                <input
-                  id="photo"
-                  name="photo"
-                  type="file"
-                  onChange={(event) => {
-                    setFieldValue('photo', event.currentTarget.files[0]); // Update form values on file selection
-                  }}
-                  className="form-control LIN-formControl"
-                />
-                <ErrorMessage name="photo" component="div" className="LIN-errorMessage" />
-              </div>
+                            <div className="form-group">
+                                <label htmlFor="datefound">Date Found</label>
+                                <input
+                                    id="datefound"
+                                    name="datefound"
+                                    type="date"
+                                    value={dateFound}
+                                    onChange={(e) => setDateFound(e.target.value)}
+                                    className="form-control LIN-formControl"
+                                />
+                                <ErrorMessage name="datefound" component="div" className="LIN-errorMessage" />
+                            </div>
 
-              <button type="submit" className="LIN-submitButton">Submit</button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </div>
-  );
+                            <div className="form-group">
+                                <label htmlFor="timefound">Time Found</label>
+                                <input
+                                    id="timefound"
+                                    name="timefound"
+                                    type="time"
+                                    value={timeFound}
+                                    onChange={(e) => setTimeFound(e.target.value)}
+                                    className="form-control LIN-formControl"
+                                />
+                                <ErrorMessage name="timefound" component="div" className="LIN-errorMessage" />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="description">Description</label>
+                                <Field id="description" name="description" type="text" className="form-control LIN-formControl" />
+                                <ErrorMessage name="description" component="div" className="LIN-errorMessage" />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Field id="email" name="email" type="email" className="form-control LIN-formControl" />
+                                <ErrorMessage name="email" component="div" className="LIN-errorMessage" />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="phone">Phone Number</label>
+                                <Field id="phone" name="phone" type="tel" className="form-control LIN-formControl" />
+                                <ErrorMessage name="phone" component="div" className="LIN-errorMessage" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Item Filter (Keyword)</label>
+                                <ReactTags
+                                    tags={tags}
+                                    handleDelete={handleDelete}
+                                    handleAddition={handleAddition}
+                                    handleDrag={handleDrag}
+                                    inputFieldPosition="bottom"
+                                    autocomplete
+                                    classNames={{
+                                        tags: 'ReactTags__tags',
+                                        tagInput: 'ReactTags__tagInput',
+                                        tagInputField: 'ReactTags__tagInputField LIN-formControl',
+                                        selected: 'ReactTags__selected',
+                                        tag: 'ReactTags__tag',
+                                        remove: 'ReactTags__remove',
+                                        suggestions: 'ReactTags__suggestions',
+                                        activeSuggestion: 'ReactTags__activeSuggestion'
+                                    }}
+                                    delimiters={[188, 13]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="photo">Photo of Item</label>
+                                <input
+                                    id="photo"
+                                    name="photo"
+                                    type="file"
+                                    onChange={(event) => {
+                                        setFieldValue('photo', event.currentTarget.files[0]); // Update form values on file selection
+                                    }}
+                                    className="form-control LIN-formControl"
+                                />
+                                <ErrorMessage name="photo" component="div" className="LIN-errorMessage" />
+                            </div>
+
+                            <button type="submit" className="LIN-submitButton">Submit</button>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
 }
 
 export default FoundItemNotice;
