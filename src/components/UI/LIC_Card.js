358555a81cef8e77703185550e5946596f20d22d
Xatan01 <149221783+Xatan01@users.noreply.github.com>
2024-04-10 23:11:20 +0800

    Added filtering and yellow marking

--- src/components/UI/LIC_Card.js
+++ src/components/UI/LIC_Card.js
@@ -1,72 +1,48 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './LIC_Card.css';
+import './LIC_Card.css'; 
 
-export default function LIC_Card(props){
-    let navigate = useNavigate();
-    const routeChange = () =>{
-      let path = "";
-      navigate(path);
-    }
+export default function LIC_Card(props) {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = "";
+    navigate(path);
+  };
 
-    const [postObject, setPostObject] = useState({});
-    const [locationObject, setlocationObject] = useState();
+  const [locationObject, setlocationObject] = useState("");
 
-    // useEffect(() =>{
-    //     axios.get('http://localhost:5000/posts/:id').then((response)=>{
-    //         setPostObject(response.data);
-    //     })
-    // })
-    // postObject.img
-    // postObject.itemTags
-    // postObject.ItemDescription
-    // postObject.LocationFound
-    const KEY = "AIzaSyB5yzIMiOUagFda-20MnNBruQAGgdsVPfc";
-    const LAT = String(props.locationFound[1]);
-    const LNG = String(props.locationFound[2]);
-    const url =`https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${KEY}`;
-    console.log(url)
+  const KEY = "AIzaSyB5yzIMiOUagFda-20MnNBruQAGgdsVPfc";
+  const LAT = String(props.locationFound[1]);
+  const LNG = String(props.locationFound[2]);
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${KEY}`;
 
-    useEffect(() => {
-        fetch(url).then((res) => {
-            return res.json();
-        }).then((data) => {
-            setlocationObject(data["results"][1]["formatted_address"]);
-        }).catch((e) => {console.log("no formatted address available")})
-    },[]);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setlocationObject(data["results"][1]["formatted_address"] || "Location Unavailable"))
+      .catch(() => console.log("Error fetching address")); 
+  }, []);
 
-    // Process JSON object
-    const tagsStringify = props.tags;
-    const tags = JSON.parse(tagsStringify);
-
-    console.log(tags);
-    console.log(Object.keys(tags))
-    console.log(tags[0])
-
-    return(
-        <Card style={{ width: '400px'}} onClick={routeChange} className="LIC-Card-Hover">
-            <Card.Img variant="top" src={props.ImageURL} className="LIC-Card-Image" />
-            <Card.Body>
-                <Card.Title style={{fontWeight: "bold"}}>{props.name}</Card.Title>
-                <Card.Text>
-                {Object.keys(tags).map((value, key) => {
-                    return(
-                    <li>{tags[key]}</li>
-                    );
-                })}
-                </Card.Text>
-            </Card.Body>
-            <ListGroup variant="flush" style={{fontStyle: "italic"}}>
-                <ListGroup.Item>
-                    Location : {locationObject}
-                </ListGroup.Item>
-            </ListGroup>
-        </Card>
-    );
+  return (
+    <Card style={{ width: '400px' }} onClick={routeChange} className="LIC-Card-Hover"> 
+      <Card.Img variant="top" src={props.ImageURL} className="LIC-Card-Image" />
+      <Card.Body>
+        <Card.Title style={{ fontWeight: "bold" }}>{props.name}</Card.Title>
+        <Card.Text>
+          {Object.keys(props.tags).map((key, index) => ( 
+            <li key={index} dangerouslySetInnerHTML={{__html: props.highlightMatch(props.tags[key], props.keyword)}} />
+          ))}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup variant="flush" style={{ fontStyle: "italic" }}>
+        <ListGroup.Item>Location: {locationObject}</ListGroup.Item>
+      </ListGroup>
+    </Card>
+  );
 }
--- src/pages/lostItemCatalog.js
+++ src/pages/lostItemCatalog.js
@@ -1,93 +1,89 @@
-import React from "react";
-import {useParams} from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import {useEffect, useState} from 'react'; // Used for api calls to server
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './lostItemCatalog.css';
 import LIC_Card from "../components/UI/LIC_Card";
-import tempImage from "../assets/questionmark-icon.png";
 import Form from 'react-bootstrap/Form';
 import { useAuth } from '../context/AuthContext'; 
 
-
-
 function LostItemCatalog() {
-    // let {id} = useParams()
-    // const [postObject, setPostObject] = useState([]); // [] since API req returns a list setListOfPosts is a function to change the lists of posts
-    // useEffect(() => {
-    //     axios.get(`http://localhost:5000/posts/byID/${id}`).then((response) => {
-    //         setPostObject(response.data);
-    //     })
-    // });
-    const { user } = useAuth();
-    let navigate = useNavigate();
-    const routeChange = () =>{
-      let path = "";
-      navigate(path);
-    }
-    // Gets all the posts in database, use .map to create that number of posts in the row.
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
-    const [listOfPosts, setListOfPosts] = useState([]);
-    useEffect(() => {
-      axios.get("http://localhost:5000/get-lost-items").then((response) => {
-        setListOfPosts(response.data["allLostItems"]);
-        console.log(response.data["allLostItems"]);
-      });
+    const { user } = useAuth();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/get-lost-items").then((response) => {
+      const allPosts = response.data["allLostItems"];
+      setListOfPosts(allPosts);
+      setFilteredPosts(allPosts); 
+    });
       if (!user) {
         navigate('/loginPage');
       }
-    }, [user, navigate]);
+  }, [user, navigate]);
 
-    
+  const highlightMatch = (tag, keyword) => {
+    if (!keyword) return tag; // If keyword is empty, return the original tag
+    const regex = new RegExp(keyword, 'gi'); // Create a case-insensitive regex
+    return tag.replace(regex, (match) => `<mark>${match}</mark>`); // Wrap matching parts in <mark> tags
+  };
+
+  const handleKeywordChange = (event) => {
+    const newKeyword = event.target.value.toLowerCase();
+    setKeyword(newKeyword);
+
+    const filtered = listOfPosts.filter(post => {
+      const tags = Object.values(post.itemFilters);  
+      return tags.some(tag => tag.toLowerCase().includes(newKeyword)); 
+    });
+
+    setFilteredPosts(filtered); 
+  };
+
+  const routeChange = () => {
+    let path = "";
+    navigate(path);
+  };
 
   return (
     <>
-      {/* Header with Filter Bar */}
-      <Container fluid style={{textAlign: "center", paddingTop: "10px"}}>
+      <Container fluid style={{ textAlign: "center", paddingTop: "10px" }}>
         <Row>
           <Col md={8}>
             <h1>Lost Items Reported</h1>
           </Col>
           <Col md={4} className="d-flex align-items-center justify-content-end">
-            <Form.Control type="text" placeholder="Keyword Filter" className="me-2" />
+            <Form.Control type="text" placeholder="Keyword Filter" className="me-2" value={keyword} onChange={handleKeywordChange}/>
           </Col>
         </Row>
       </Container>
 
-      {/* Item Catalog */}
-      <Container fluid className="LIC-Catalog" style={{paddingTop: "20px"}}>
+      <Container fluid className="LIC-Catalog" style={{ paddingTop: "20px" }}>
         <Row>
-          {listOfPosts.map((value, key) => {
-            return (
-              <Col xs={4} className="LIC-Flexbox" onClick={() => navigate(`/LostItemDescription/${key}`)}>
-                <LIC_Card name={value.name} tags={JSON.stringify(value.itemFilters)} locationFound={value.location} ImageURL={value.ImageURL}/>
-              </Col>
-            );
-            })
-          }
-          
-          {/* <Col xs={4} className="LIC-Flexbox" onClick={() => navigate("/LostItemDescription/:id")}>
-            <LIC_Card/>
-          </Col>
-          <Col xs={4} className="LIC-Flexbox" onClick={() => navigate("/LostItemDescription/:id")}>
-            <LIC_Card/>
-          </Col>
-          <Col xs={4} className="LIC-Flexbox" onClick={() => navigate("/LostItemDescription/:id")}>
-            <LIC_Card/>
-          </Col> */}
-
+          {filteredPosts.map((value, key) => (
+            <Col xs={4} className="LIC-Flexbox" key={key} onClick={() => navigate(`/LostItemDescription/${key}`)}>
+              <LIC_Card 
+                name={value.name} 
+                tags={value.itemFilters} 
+                locationFound={value.location} 
+                ImageURL={value.ImageURL} 
+                keyword={keyword} 
+                highlightMatch={highlightMatch} 
+              /> 
+            </Col>
+          ))}
         </Row>
       </Container>
 
-      {/* Buttons */}
-      <Container fluid style={{textAlign: "center", margin: "20px 0", paddingBottom: "20px"}}>
+      <Container fluid style={{ textAlign: "center", margin: "20px 0", paddingBottom: "20px" }}>
         <Row>
           <Col>
             <Button variant="primary" href="/lostItemMap" onClick={() => routeChange('/map-view')}>Return to Map View</Button>
@@ -98,7 +94,7 @@ function LostItemCatalog() {
         </Row>
       </Container>
     </>
-  )
+  );
 };
 
 export default LostItemCatalog;
